c766071cd3e50e17570865f730f4cec54d818e7b
hosseinvejdani <vejdani.shojae@gmail.com>
2022-07-29 01:55:08 -0700

    add important parts

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import  FastAPI
 
 from .v1.routers import  users as users_v1
 from .v2.routers import  users as users_v2
+from .v2.routers import  sms as sms_v2
+from .v2.routers import  application as application_v2
 
 app = FastAPI()
 
@@ -10,6 +12,8 @@ app.include_router(users_v1.router)
 # # ---------------------------------
 v2 = FastAPI()
 v2.include_router(users_v2.router)
+v2.include_router(sms_v2.router)
+v2.include_router(application_v2.router)
 
 # ---------
 app.mount("/api/v2", v2)
